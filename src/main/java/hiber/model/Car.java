f779164556e40c180b8922966b2129c49f5955e6
Pavel Okoemov <pasha.okoyomov@mail.ru>
2026-03-15 13:05:49 +0300

    Added Car class, configured DB connection

---
+++ src/main/java/hiber/model/Car.java
@@ -0,0 +1,49 @@
+package hiber.model;
+
+import java.util.Objects;
+
+public class Car {
+    private String model;
+    int series;
+    public Car() {}
+    public Car(int series, String model) {
+        this.series = series;
+        this.model = model;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getSeries() {
+        return series;
+    }
+
+    public void setSeries(int series) {
+        this.series = series;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Car)) return false;
+        Car car = (Car) o;
+        return series == car.series && Objects.equals(model, car.model);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(model, series);
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "model='" + model + '\'' +
+                ", series=" + series +
+                '}';
+    }
+}
